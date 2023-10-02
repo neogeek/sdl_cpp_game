460192adc0a9845c7766f8976de5eba7f6abb805
Scott Doxey <neogeek0101@gmail.com>
2023-10-01 23:55:19 -0400

    Added header blocks.

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -8,15 +8,25 @@ GameObject::GameObject() {}
 GameObject::GameObject(SDL_Renderer *_renderer) : renderer(_renderer) {}
 GameObject::GameObject(SDL_Renderer *_renderer, SDL_Rect *_rect) : renderer(_renderer), rect(_rect) {}
 
+/**
+ * Set rect position and size to the GameObject.
+ * @param rect A rectangle, with the origin at the upper left (integer).
+ */
 void GameObject::SetRect(SDL_Rect *_rect)
 {
     rect = _rect;
 }
 
+/**
+ * Render GameObject to the scene.
+ */
 void GameObject::Render()
 {
 }
 
+/**
+ * Cleanup function to run after the scene is unloaded.
+ */
 void GameObject::Clean()
 {
 }
--- src/ImageRenderObject.cpp
+++ src/ImageRenderObject.cpp
@@ -7,11 +7,22 @@
 ImageRenderObject::ImageRenderObject(SDL_Renderer *_renderer) : GameObject(_renderer) {}
 ImageRenderObject::ImageRenderObject(SDL_Renderer *_renderer, SDL_Rect *_rect) : GameObject(_renderer, _rect) {}
 
+/**
+ * Load textures by path.
+ * @param path Path to a local texture.
+ *
+ * @deprecated DEVELOPMENT USE ONLY! Use LoadTextureRW to load textures in a final build.
+ */
 void ImageRenderObject::LoadTexture(const char *path)
 {
     texture = SDL_Image_Utilities::LoadTexture(renderer, path);
 }
 
+/**
+ * Load textures from memory buffer.
+ * @param mem Pointer to a read-only buffer. (texture loaded in via header file)
+ * @param size Length of a buffer in bytes.
+ */
 void ImageRenderObject::LoadTextureRW(const void *mem, int size)
 {
     texture = SDL_Image_Utilities::LoadTextureRW(renderer, mem, size);
