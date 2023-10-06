af4acb67216d44c100c5ce5a15640b1cd9a3b8c8
Scott Doxey <neogeek0101@gmail.com>
2023-10-05 23:37:00 -0400

    Added a custom webgl shell.

--- bin/build_webgl.sh
+++ bin/build_webgl.sh
@@ -3,4 +3,5 @@
 mkdir -p build/webgl
 
 emcc -std=c++17 -o build/webgl/index.html src/*.cpp -Ifonts -Iimages -Iincludes \
-    -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2
+    -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 \
+    --shell-file templates/webgl_shell.html
