6afd0f2e0412e71a06850b39aadfd50568dbbd91
Scott Doxey <neogeek0101@gmail.com>
2023-10-11 00:12:19 -0400

    Added Roxzilla game object.

---
+++ src/Roxzilla.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "../images/roxzilla-idle.h"
+
+#include "Game.hpp"
+#include "ImageRenderObject.hpp"
+
+class Roxzilla
+{
+
+  private:
+    Game *game;
+
+    int frame = 1;
+
+    double nextTick;
+
+    const double frameSpeed = 0.15;
+
+  public:
+    std::unique_ptr<ImageRenderObject> image;
+
+    Roxzilla(Game *_game) : game(_game)
+    {
+        image = std::make_unique<ImageRenderObject>();
+
+        image->LoadTextureRW(game->GetRenderer(), images_roxzilla_idle_png,
+                             images_roxzilla_idle_png_len);
+
+        image->SetScale(5);
+
+        image->SetSrcRect(0, 0, 65, 83);
+
+        image->SetRect(25, 25, 65, 83);
+
+        image->SetUpdate(
+            [this](GameObject *ref, double deltaTime)
+            {
+                nextTick += deltaTime;
+
+                if (nextTick < frameSpeed)
+                {
+                    return;
+                }
+
+                ImageRenderObject *imageObj =
+                    dynamic_cast<ImageRenderObject *>(ref);
+
+                imageObj->SetSrcRect(59 * (frame - 1), 0, 59, 80);
+
+                frame += 1;
+
+                if (frame > 6)
+                {
+                    frame = 1;
+                }
+
+                nextTick = 0;
+            });
+    }
+};
