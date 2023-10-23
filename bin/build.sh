6a920638807e756223f986f1ee1a4acf1d8d0a54
Scott Doxey <neogeek0101@gmail.com>
2023-10-22 23:03:23 -0400

    Updated sdl version.

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 mkdir -p build/
 
 g++ -std=c++17 -o build/sdl_cpp_game src/*.cpp -Ifonts -Iimages -Iincludes -Isrc \
-    -I/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.28.3/lib \
+    -I/opt/homebrew/Cellar/sdl2/2.28.4/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.28.4/lib \
     -I/opt/homebrew/Cellar/sdl2_image/2.6.3_2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.6.3_2/lib \
     -I/opt/homebrew/Cellar/sdl2_ttf/2.20.2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.20.2/lib \
     -lSDL2 -lSDL2_image -lSDL2_ttf
