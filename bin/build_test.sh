f9cae4cf5775846cfefe714047628958a20c4bae
Scott Doxey <neogeek0101@gmail.com>
2024-04-04 13:37:33 -0400

    Updated SDL.

--- bin/build_test.sh
+++ bin/build_test.sh
@@ -3,8 +3,8 @@
 mkdir -p build/
 
 g++ -std=c++17 -o build/sdl_cpp_game_test src/*.cpp -Ifonts -Iimages -Iincludes \
-    -I/opt/homebrew/Cellar/sdl2/2.28.4/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.28.4/lib \
-    -I/opt/homebrew/Cellar/sdl2_image/2.6.3_2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.6.3_2/lib \
-    -I/opt/homebrew/Cellar/sdl2_ttf/2.20.2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.20.2/lib \
+    -I/opt/homebrew/Cellar/sdl2/2.30.2/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.2/lib \
+    -I/opt/homebrew/Cellar/sdl2_image/2.8.2_1/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_1/lib \
+    -I/opt/homebrew/Cellar/sdl2_ttf/2.22.0/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.22.0/lib \
     -lSDL2 -lSDL2_image -lSDL2_ttf \
     -fsanitize=address
