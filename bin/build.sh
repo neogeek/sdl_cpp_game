bc8c8c5d32e8243e4d7504f41a189bdf1841eb7e
Scott Doxey <neogeek0101@gmail.com>
2024-09-09 13:38:15 -0400

    Updated SDL.

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 mkdir -p build/
 
 g++ -std=c++17 -o build/sdl_cpp_game src/*.cpp -Ifonts -Iimages -Iincludes -Isrc \
-    -I/opt/homebrew/Cellar/sdl2/2.30.6/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.6/lib \
+    -I/opt/homebrew/Cellar/sdl2/2.30.7/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.7/lib \
     -I/opt/homebrew/Cellar/sdl2_image/2.8.2_1/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_1/lib \
     -I/opt/homebrew/Cellar/sdl2_ttf/2.22.0/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.22.0/lib \
     -framework CoreMIDI -framework CoreFoundation \
