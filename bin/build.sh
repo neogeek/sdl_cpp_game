0eb34a36c99674725040047db1be8aa1556d00e1
Scott Doxey <neogeek0101@gmail.com>
2024-10-04 11:53:40 -0400

    Updated SDL.

--- bin/build.sh
+++ bin/build.sh
@@ -3,8 +3,8 @@
 mkdir -p build/
 
 g++ -std=c++17 -o build/sdl_cpp_game src/*.cpp -Ifonts -Iimages -Iincludes -Isrc \
-    -I/opt/homebrew/Cellar/sdl2/2.30.7/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.7/lib \
-    -I/opt/homebrew/Cellar/sdl2_image/2.8.2_1/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_1/lib \
+    -I/opt/homebrew/Cellar/sdl2/2.30.8/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.8/lib \
+    -I/opt/homebrew/Cellar/sdl2_image/2.8.2_2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_2/lib \
     -I/opt/homebrew/Cellar/sdl2_ttf/2.22.0/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.22.0/lib \
     -framework CoreMIDI -framework CoreFoundation \
     -lSDL2 -lSDL2_image -lSDL2_ttf
--- bin/build_test.sh
+++ bin/build_test.sh
@@ -2,9 +2,10 @@
 
 mkdir -p build/
 
-g++ -std=c++17 -o build/sdl_cpp_game_test src/*.cpp -Ifonts -Iimages -Iincludes \
-    -I/opt/homebrew/Cellar/sdl2/2.30.6/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.6/lib \
-    -I/opt/homebrew/Cellar/sdl2_image/2.8.2_1/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_1/lib \
+g++ -std=c++17 -o build/sdl_cpp_game src/*.cpp -Ifonts -Iimages -Iincludes -Isrc \
+    -I/opt/homebrew/Cellar/sdl2/2.30.8/include/SDL2 -L/opt/homebrew/Cellar/sdl2/2.30.8/lib \
+    -I/opt/homebrew/Cellar/sdl2_image/2.8.2_2/include/SDL2 -L/opt/homebrew/Cellar/sdl2_image/2.8.2_2/lib \
     -I/opt/homebrew/Cellar/sdl2_ttf/2.22.0/include/SDL2 -L/opt/homebrew/Cellar/sdl2_ttf/2.22.0/lib \
+    -framework CoreMIDI -framework CoreFoundation \
     -lSDL2 -lSDL2_image -lSDL2_ttf \
     -fsanitize=address
