8897a781e466716f3638e6f3c368d1027ada1f64
Scott Doxey <neogeek0101@gmail.com>
2025-03-12 22:18:03 -0400

    Updated compile static assets script.

--- bin/compile-static-assets.sh
+++ bin/compile-static-assets.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
     cd ..
 
-    find images -type f -name "*.png" -exec sh -c 'xxd -i "$0" "${0%.png}.h"' {} \;
-    find fonts -type f -name "*.ttf" -exec sh -c 'xxd -i "$0" "${0%.ttf}.h"' {} \;
+    find fonts -type f -name "*.ttf" -exec sh -c 'echo "#pragma once\n" > "${0%.ttf}.h" && xxd -i "$0" >> "${0%.ttf}.h"' {} \;
+    find images -type f -name "*.png" -exec sh -c 'echo "#pragma once\n" > "${0%.png}.h" && xxd -i "$0" >> "${0%.png}.h"' {} \;
 
 )
